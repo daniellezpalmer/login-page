eed54b41960498335d6f780bc8f09d557ef4f76c
Danielle Palmer <daniellezpalmer@gmail.com>
2017-08-17 16:01:27 -0500

    Created log in page

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-const bodyParser = require('body-parser');
 const express = require('express');
-// const expressValidator = require('express-validator');
 const mustache = require('mustache');
 const mustacheExpress = require('mustache-express');
 const app = express();
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(expressValidator());
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
 app.get('/', function (req, res) {
-  res.send('home')
+  res.render('login')
 })
 
 app.listen(3000, function () {
