70ffe52310ca1bc0496d98f79219c4f56ef7456b
Danielle Palmer <daniellezpalmer@gmail.com>
2017-08-17 16:34:44 -0500

    installed session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mustache = require('mustache');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
+const session = require('express-session')
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -10,13 +11,19 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+1
 app.use(function (req, res, next) {
   console.log('in interceptor');
   next()
 })
 
 app.get('/', function (req, res) {
-  res.render('login')
+  res.render('home')
 })
 
 app.post('/login', function (req, res) {
