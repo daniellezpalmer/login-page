8502b42196c04f07d2abd6f9df881e8b79edaae8
Danielle Palmer <daniellezpalmer@gmail.com>
2017-08-17 16:16:24 -0500

    posted to login

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mustache = require('mustache');
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
@@ -9,6 +12,12 @@ app.set('view engine', 'mustache');
 
 app.get('/', function (req, res) {
   res.render('login')
+})
+
+app.post('/login', function (req, res) {
+  console.log('username is: ' + req.body.username);
+  console.log('password is: ' + req.body.password);
+  res.render('home')
 })
 
 app.listen(3000, function () {
